4216776fd3c967546f11883323526961ef3f7011
pefish <darrondarron@qq.com>
2023-06-07 10:06:30 +0800

    v0.0.1

---
+++ client.go
@@ -0,0 +1,146 @@
+package btc_rpc_client
+
+import (
+	"fmt"
+	go_decimal "github.com/pefish/go-decimal"
+	go_http "github.com/pefish/go-http"
+	go_logger "github.com/pefish/go-logger"
+	"time"
+)
+
+type BtcRpcClient struct {
+	timeout  time.Duration
+	logger   go_logger.InterfaceLogger
+	baseUrl  string
+	username string
+	password string
+}
+
+func NewBtcRpcClient(
+	logger go_logger.InterfaceLogger,
+	httpTimeout time.Duration,
+	baseUrl string,
+	username string,
+	password string,
+) *BtcRpcClient {
+	return &BtcRpcClient{
+		timeout:  httpTimeout,
+		logger:   logger,
+		baseUrl:  baseUrl,
+		username: username,
+		password: password,
+	}
+}
+
+type GetRawTransactionResult struct {
+	Vin []struct {
+		TxId string `json:"txid"`
+		Vout uint64 `json:"vout"`
+	} `json:"vin"`
+	Vout []struct {
+		Value        float64 `json:"value"`
+		N            uint64  `json:"n"`
+		ScriptPubKey struct {
+			Address string `json:"address"`
+			Type    string `json:"type"`
+		} `json:"scriptPubKey"`
+	} `json:"vout"`
+	Confirmations uint64 `json:"confirmations,omitempty"`
+}
+
+func (brc *BtcRpcClient) GetRawTransaction(hash string) (*GetRawTransactionResult, error) {
+	var result struct {
+		Result *GetRawTransactionResult `json:"result"`
+		Error  *string                  `json:"error"`
+	}
+	_, err := go_http.NewHttpRequester(go_http.WithTimeout(brc.timeout), go_http.WithLogger(brc.logger)).PostForStruct(go_http.RequestParam{
+		Url: brc.baseUrl,
+		Params: map[string]interface{}{
+			"jsonrpc": "2.0",
+			"method":  "getrawtransaction",
+			"params": []interface{}{
+				hash,
+				1,
+			},
+			"id": 1,
+		},
+		BasicAuth: &go_http.BasicAuth{
+			Username: brc.username,
+			Password: brc.password,
+		},
+	}, &result)
+	if err != nil {
+		return nil, err
+	}
+	if result.Error != nil {
+		return nil, fmt.Errorf(*result.Error)
+	}
+	return result.Result, nil
+}
+
+type ListTransactionsResult struct {
+	TxId string `json:"txid"`
+}
+
+func (brc *BtcRpcClient) ListTransactions(index uint64, address string) ([]ListTransactionsResult, error) {
+	var result struct {
+		Result []ListTransactionsResult `json:"result"`
+		Error  *string                  `json:"error"`
+	}
+	_, err := go_http.NewHttpRequester(go_http.WithTimeout(brc.timeout), go_http.WithLogger(brc.logger)).PostForStruct(go_http.RequestParam{
+		Url: brc.baseUrl,
+		Params: map[string]interface{}{
+			"jsonrpc": "2.0",
+			"method":  "listtransactions",
+			"params": []interface{}{
+				address,
+				10,
+				index,
+				true,
+			},
+			"id": 1,
+		},
+		BasicAuth: &go_http.BasicAuth{
+			Username: brc.username,
+			Password: brc.password,
+		},
+	}, &result)
+	if err != nil {
+		return nil, err
+	}
+	if result.Error != nil {
+		return nil, fmt.Errorf(*result.Error)
+	}
+	return result.Result, nil
+}
+
+func (brc *BtcRpcClient) EstimateSmartFee() (string, error) {
+	var result struct {
+		Result struct {
+			Feerate float64 `json:"feerate"`
+		} `json:"result"`
+		Error *string `json:"error"`
+	}
+	_, err := go_http.NewHttpRequester(go_http.WithTimeout(brc.timeout), go_http.WithLogger(brc.logger)).PostForStruct(go_http.RequestParam{
+		Url: brc.baseUrl,
+		Params: map[string]interface{}{
+			"jsonrpc": "2.0",
+			"method":  "estimatesmartfee",
+			"params": []interface{}{
+				10,
+			},
+			"id": 1,
+		},
+		BasicAuth: &go_http.BasicAuth{
+			Username: brc.username,
+			Password: brc.password,
+		},
+	}, &result)
+	if err != nil {
+		return "", err
+	}
+	if result.Error != nil {
+		return "", fmt.Errorf(*result.Error)
+	}
+	return go_decimal.Decimal.Start(result.Result.Feerate).MustShiftedBy(5).EndForString(), nil
+}
